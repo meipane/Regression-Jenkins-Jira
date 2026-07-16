2d58c677444d183322097e08cf06c03f15b4a8ed
meipane <82637135+meipane@users.noreply.github.com>
2026-07-16 18:08:39 +0700

    fix docker

--- ui/src/test/java/utils/BaseTest.java
+++ ui/src/test/java/utils/BaseTest.java
@@ -16,23 +16,36 @@ public class BaseTest {
             WebDriverManager.chromedriver().setup();
 
             ChromeOptions options = new ChromeOptions();
+
+            // Untuk Jenkins Docker
             options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--disable-gpu");
+            options.addArguments("--disable-software-rasterizer");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--window-size=1920,1080");
+
+            // Paksa Chrome menggunakan binary Linux
+            options.setBinary("/usr/bin/google-chrome");
 
             driver = new ChromeDriver(options);
         }
     }
 
+
     public static WebDriver getDriver() {
+
         if (driver == null) {
             initializeDriver();
         }
+
         return driver;
     }
 
+
     public static void quitDriver() {
+
         if (driver != null) {
             driver.quit();
             driver = null;
