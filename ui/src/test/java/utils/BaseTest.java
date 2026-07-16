200b8ca37f0c2a4f1bf29a3577c6483b25e86a6d
meipane <82637135+meipane@users.noreply.github.com>
2026-07-16 17:28:06 +0700

    update lagi

--- ui/src/test/java/utils/BaseTest.java
+++ ui/src/test/java/utils/BaseTest.java
@@ -3,15 +3,24 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
+
     protected static WebDriver driver;
 
     public static void initializeDriver() {
         if (driver == null) {
+
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(options);
         }
     }
 
